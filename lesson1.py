2e4a4f0d8b627fa0bd7a8cdeec58024a98fe5017
Timur Kasimov <urmanit@gmail.com>
2021-09-29 04:59:09 -0400

    First lesson ex2

--- lesson1.py
+++ lesson1.py
@@ -7,3 +7,12 @@ print ('Letter "S" is: ',log_str.lower().count('s')) #1.5
 c = (log_str[7:15]).replace(':', '+') #1.6
 c = eval(c)
 print ('Sum three digits = ',c)
+
+new_log = 'May 24 14:03:01 ideapad colord[844]: failed to get session [pid 8279]: Нет доступных данных'
+pc_name = new_log[16:23]
+service_name =new_log[24:35]
+msg =new_log[37:69]
+err = new_log[70:]
+date_time =new_log[:15]
+print(f'The PC {pc_name} receive message from service {service_name} what says {msg} because {err} at {date_time}')
+
