300f2295ab2fd4eb666f227fd8747cdd3a6efbdc
Timur Kasimov <urmanit@gmail.com>
2021-10-29 10:26:06 -0400

    Add lesson2.py- work with time STD lib Python

--- lesson1.py
+++ lesson1.py
@@ -1,22 +1,4 @@
-log_str = 'May 24 12:48:31 ideapad systemd[1]: logrotate.service: Succeeded.'
-print (log_str[:15]) #1.1
-print (log_str[24:34]) #1.2
-print (log_str.replace('ideapad', 'PC-12092')) #1.3
-print (log_str.find('failed')) #1.4
-print ('Letter "S" is: ',log_str.lower().count('s')) #1.5
-c = (log_str[7:15]).replace(':', '+') #1.6
-c = eval(c)
-print ('Sum three digits = ',c)
-
-new_log = 'May 24 14:03:01 ideapad colord[844]: failed to get session [pid 8279]: Нет доступных данных'
-pc_name = new_log[16:23]
-service_name =new_log[24:35]
-msg =new_log[37:69]
-err = new_log[70:]
-date_time =new_log[:15]
-print(f'The PC {pc_name} receive message from service {service_name} what says {msg} because {err} at {date_time}')
-
-str1 = ['
-
-
-
+s = ['May 18 11:59:18 PC-00102 plasmashell[1312]: kf.plasma.core: findInCache with a lastModified timestamp of 0 is deprecated', 'May 18 13:06:54 ideapad kwin_x11[1273]: Qt Quick Layouts: Detected recursive rearrange. Aborting after two iterations.', 'May 20 09:16:28 PC0078 systemd[1]: Starting PackageKit Daemon...', 'May 20 11:01:12 PC-00102 PackageKit: daemon start', 'May 20 12:48:18 PC0078 systemd[1]: Starting Message of the Day...', 'May 21 14:33:55 PC0078 kernel: [221558.992188] usb 1-4: New USB device found, idVendor=1395, idProduct=0025, bcdDevice= 1.00', 'May 22 11:48:30 ideapad mtp-probe: checking bus 1, device 3: "/sys/devices/pci0000:00/0000:00:08.1/0000:03:00.3/usb1/1-4"', 'May 22 11:50:09 ideapad mtp-probe: bus: 1, device: 3 was not an MTP device', 'May 23 08:06:14 PC-00233 kernel: [221559.381614] usbcore: registered new interface driver snd-usb-audio', 'May 24 16:19:52 PC-00233 systemd[1116]: Reached target Sound Card.', 'May 24 19:26:40 PC-00102 rtkit-daemon[1131]: Supervising 5 threads of 2 processes of 1 users.']
+for n in s[0]:
+    mc = (n.split())
+    print(n)
